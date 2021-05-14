14d6e52aeff1af4a32db0c78a74bc1bcafda69db
Decave <decave27@gmail.com>
2021-05-15 03:10:29 +0900

    style:prettier format

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,28 @@
-import * as express from 'express'
-import * as dotenv from 'dotenv'
-import * as cors from 'cors'
-import router from '../routes'
+import * as express from "express";
+import * as dotenv from "dotenv";
+import * as cors from "cors";
+import router from "../routes";
 
-dotenv.config({ path: '../.env' })
-const app = express()
-const port = process.env.PORT || 8080
+dotenv.config({ path: "../.env" });
+const app = express();
+const port = process.env.PORT || 8080;
 const corsOptions = {
-    origin: '*'
-}
+  origin: "*",
+};
 const urlencodedOptions = {
-    extended: true
-}
+  extended: true,
+};
 
-app.use(router)
-app.use(cors(corsOptions))
-app.use(express.json())
-app.use(express.urlencoded(urlencodedOptions))
-app.set('trust proxy', true)
+app.use(router);
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded(urlencodedOptions));
+app.set("trust proxy", true);
 
 app.listen(port, () => {
-    console.log('\x1b[36m%s\x1b[34m%s\x1b[0m', '[Server]',`  Server on  : ${port}`)
-})
+  console.log(
+    "\x1b[36m%s\x1b[34m%s\x1b[0m",
+    "[Server]",
+    `  Server on  : ${port}`
+  );
+});
