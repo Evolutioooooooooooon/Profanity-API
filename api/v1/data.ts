27b622f716a0245783b8023e45a401491a1dbf0a
Decave <decave27@gmail.com>
2021-05-15 03:07:32 +0900

    refactor: prettier, send html codes

--- api/v1/data.ts
+++ api/v1/data.ts
@@ -1,22 +1,25 @@
-import { Request, Response } from 'express'
-import getData from '../../utils/getData'
-const words = require('../../assets/data.json')
+import { Request, Response } from "express";
+import getData from "../../utils/getData";
+const words = require("../../assets/data.json");
 
 const data = (req: Request, res: Response) => {
-    if(!req.query.message) return res.send('Error: You need to give a message parameter value. \n2021 Team Sirius - Chul0721. © All Rights Reserved.')
-    let apiData = words.data
-    function x() {
-        for(let i = 0; i < apiData.length; i++) {
-            let result = getData(i, apiData, req.query.message)
-            if(!result) return false
-        }
-        return true
+  if (!req.query.message)
+    return res.send(
+      "<h1>Error: You need to give a message parameter value. </h1><br><h5>2021 Team Sirius - Chul0721. © All Rights Reserved.</h5>"
+    );
+  let apiData = words.data;
+  function x() {
+    for (let i = 0; i < apiData.length; i++) {
+      let result = getData(i, apiData, req.query.message);
+      if (!result) return false;
     }
-    if(x()) {
-        return res.send('clean')
-    } else {
-        return res.send('cuss')
-    }
-}
+    return true;
+  }
+  if (x()) {
+    return res.send("clean");
+  } else {
+    return res.send("cuss");
+  }
+};
 
-export default data
+export default data;
