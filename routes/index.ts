1a263fe00ddaadbcad72d05f25198bf4a41c350e
Decave <decave27@gmail.com>
2021-05-15 03:09:51 +0900

    style: prettier format

--- routes/index.ts
+++ routes/index.ts
@@ -1,11 +1,11 @@
-import { Router } from 'express'
-import { main, data, notFound, postRequest } from '../api'
+import { Router } from "express";
+import { main, data, notFound, postRequest } from "../api";
 
-const router = Router()
+const router = Router();
 
-router.get('/api/v1/', main)
-router.get('/api/v1/data', data)
-router.get('*', notFound)
-router.post('*', postRequest)
+router.get("/api/v1/", main);
+router.get("/api/v1/data", data);
+router.get("*", notFound);
+router.post("*", postRequest);
 
-export default router
+export default router;
